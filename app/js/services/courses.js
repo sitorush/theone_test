4307a18c23e29aa36dcfe1f9f273f143b334c812
sitorush <tom.marulak@gorkana.com>
2016-04-27 13:57:56 +0100

    remove commentted code

--- app/js/services/courses.js
+++ app/js/services/courses.js
@@ -4,14 +4,6 @@ function CoursesService($http) {
   const service = {};
 
   service.getCourses = function() {
-    // return new Promise((resolve, reject) => {
-    //   $http.get('/mock/courses.json').success((data) => {
-    //     resolve(data);
-    //   }).error((err, status) => {
-    //     reject(err, status);
-    //   });
-    // });
-
     return $http.get('/mock/courses.json');
   };
 
